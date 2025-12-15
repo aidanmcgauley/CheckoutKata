f51a53cb0f0c215fb31199d9fc6b2859022ffd35
aidanmcgauley <aidanmcgauley@gmail.com>
2025-12-15 14:29:28 +0000

    added the PricingRule model class

--- CheckoutKata/Pricing/PricingRule.cs
+++ CheckoutKata/Pricing/PricingRule.cs
@@ -2,5 +2,24 @@
 
 public class PricingRule
 {
-    
+    public string Sku  { get; set; }
+    public int UnitPrice { get; set; }
+    public int? SpecialQuantity { get; set; }
+    public int? SpecialPrice { get; set; }
+
+    public PricingRule(string sku, int unitPrice)
+    {
+        Sku = sku;
+        UnitPrice = unitPrice;
+        SpecialQuantity = null;
+        SpecialPrice = null;
+    }
+
+    public PricingRule(string sku, int unitPrice, int specialQuantity, int specialPrice)
+    {
+        Sku = sku;
+        UnitPrice = unitPrice;
+        SpecialQuantity = specialQuantity;
+        SpecialPrice = specialPrice;
+    }
 }
