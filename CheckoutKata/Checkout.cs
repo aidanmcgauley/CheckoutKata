777dcb041f02d3d298d335c0bf78f073b975b80f
aidanmcgauley <aidanmcgauley@gmail.com>
2025-12-15 15:03:02 +0000

    added constructor for Checkout, and implemented Scan method

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -1,6 +1,25 @@
-﻿namespace CheckoutKata;
+﻿using CheckoutKata.Interfaces;
+using CheckoutKata.Pricing;
 
-public class Checkout
+namespace CheckoutKata;
+
+public class Checkout : ICheckout
 {
+    public PricingRules _pricingRules;
+    public Dictionary<string, int> _itemCounts = new Dictionary<string, int>();
+
+    public Checkout(PricingRules pricingRules)
+    {
+        _pricingRules = pricingRules;
+    }
     
+    public void Scan(string item)
+    {
+        _itemCounts[item] = _itemCounts.GetValueOrDefault(item, 0)+1;
+    }
+
+    public int GetTotalPrice()
+    {
+        throw new NotImplementedException();
+    }
 }
