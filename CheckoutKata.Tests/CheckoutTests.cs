73f6a68bc0b73cb7e0e99552e7e06672fc910b18
aidanmcgauley <aidanmcgauley@gmail.com>
2025-12-15 14:14:35 +0000

    added failing tests to cover basic expected checkout scenarios

--- CheckoutKata.Tests/CheckoutTests.cs
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -8,7 +8,49 @@ public class CheckoutTests
     }
 
     [Test]
-    public void Test1()
+    public void Empty_checkout_returns_zero()
+    {
+        Assert.Fail();
+    }
+    
+    [Test]
+    public void Single_item_A_costs_50()
+    {
+        Assert.Fail();
+    }
+    
+    [Test]
+    public void Multiple_items_no_special_pricing()
+    {
+        Assert.Fail();
+    }
+    
+    [Test]
+    public void Three_As_cost_130()
+    {
+        Assert.Fail();
+    }
+    
+    [Test]
+    public void Four_As_cost_180()
+    {
+        Assert.Fail();
+    }
+    
+    [Test]
+    public void Six_As_cost_260()
+    {
+        Assert.Fail();
+    }
+    
+    [Test]
+    public void Mixed_special_and_non_special_items_are_priced_correctly()
+    {
+        Assert.Fail();
+    }
+    
+    [Test]
+    public void Total_is_updated_incrementally_as_items_are_scanned()
     {
         Assert.Fail();
     }
